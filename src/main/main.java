f189f9d2943fb89dd53db27b012c2e2cfa30950e
victorbillaud <victorbillaud@gmail.com>
2021-11-15 16:01:37 +0100

    Ajout Joueur et Joueurs

--- src/main/main.java
+++ src/main/main.java
@@ -3,9 +3,10 @@ import static questions.TypeQuestion.*;
 
 public class main {
 	public static void main (String[] args) {
+		String alphabet[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 		Joueurs Game = new Joueurs();
 		for (int i = 0; i < Game.getJoueurs().length; i++) {
-			Game.getJoueurs()[i] = new Joueur(100+10*i, Integer.toString(i), 0,Status.ATTENTE);
+			Game.getJoueurs()[i] = new Joueur(100+10*i, alphabet[i], 0,Status.ATTENTE);
 		}
 		Game.generateParticipants();
 		System.out.println(Game);
